9fe265ca4c76f547bd454b11ed2e8f602daeda65
MatteoVH <matteovh@ucla.edu>
2015-09-26 17:01:12 -0700

    add smooth-scrolling for nav links and hidden description sections

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('.site-img').click(function() {
+		$(this).next().toggle(200);
+	});
+	$('a[href*=#]').on('click', function(event){     
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 400);
+	});
+});
